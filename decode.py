ef5c7c3e903ea45527e59366d9c91c94ecb40a81
Angelos Moavinis <amoavinis@gmail.com>
2022-06-09 21:35:53 +0300

    wip

--- decode.py
+++ decode.py
@@ -1,14 +1,18 @@
 import os
 import json
+from random import sample, seed
 
 lines = []
 
-for dir1 in os.listdir("Datasets/ThessalonikiDataset/data"):
-    for file in os.listdir("Datasets/ThessalonikiDataset/data/"+dir1+"/"):
-        for line in open("Datasets/ThessalonikiDataset/data/"+dir1+"/"+file, "r").readlines():
+for dir1 in os.listdir("Datasets/ThessalonikiDataset2G/data"):
+    for file in os.listdir("Datasets/ThessalonikiDataset2G/data/"+dir1+"/"):
+        for line in open("Datasets/ThessalonikiDataset2G/data/"+dir1+"/"+file, "r").readlines():
             lines.append(line)
 
 print(len(lines))
+seed(100)
+
+lines = sample(lines, 100000)
 
 data = []
 for line in lines:
@@ -16,6 +20,6 @@ for line in lines:
     data.append(obj["frame"])
 
 #print(data[:3])
-with open("encoded.txt", "w") as file:
+with open("encoded2G.txt", "w") as file:
     for d in data:
         file.write(d+"\n")
